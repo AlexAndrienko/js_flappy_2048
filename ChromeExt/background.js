c82f75b120b0c8c48eacee0e1619344e5de60310
AlexAndrienko <alex.ddevil.andrienko@gmail.com>
2014-05-06 00:44:58 +0300

    Ported to launch as chrome app

---
+++ ChromeExt/background.js
@@ -0,0 +1,8 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('window.html', {
+        'bounds': {
+            'width': 840,
+            'height': 560
+        }
+    });
+});
